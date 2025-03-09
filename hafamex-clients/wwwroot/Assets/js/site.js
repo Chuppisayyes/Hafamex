8b1b18b48d53f269aa4a35fea48640d9351d1895
Chuppisayyes <phtnhvan8624@gmail.com>
2025-03-09 17:53:05 +0700

    PUSH_HomePage

---
+++ hafamex-clients/wwwroot/Assets/js/site.js
@@ -0,0 +1,20 @@
+window.initCounterAnimation = function () {
+    const counters = document.querySelectorAll(".counter");
+
+    counters.forEach(counter => {
+        let target = +counter.getAttribute("data-target");
+        let count = 0;
+        let speed = target / 100; // Điều chỉnh tốc độ đếm
+
+        function updateCount() {
+            count += speed;
+            if (count < target) {
+                counter.innerText = Math.floor(count);
+                requestAnimationFrame(updateCount);
+            } else {
+                counter.innerText = target;
+            }
+        }
+        updateCount();
+    });
+};

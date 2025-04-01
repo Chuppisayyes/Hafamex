c5a55fd58233a3abdadd4351f8fbbd7b042e0425
THIEN BAO <baotruong.31221020819@st.ueh.edu.vn>
2025-04-02 00:28:15 +0700

    contact-about

--- hafamex-clients/obj/Debug/net9.0/hafamex-clients.AssemblyInfo.cs
+++ hafamex-clients/obj/Debug/net9.0/hafamex-clients.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hafamex-clients")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1b18b48d53f269aa4a35fea48640d9351d1895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a911da3e831ee9e5ff44c3406482be95d5171551")]
 [assembly: System.Reflection.AssemblyProductAttribute("hafamex-clients")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hafamex-clients")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- hafamex-clients/wwwroot/Assets/js/site.js
+++ hafamex-clients/wwwroot/Assets/js/site.js
@@ -1,20 +1,31 @@
 window.initCounterAnimation = function () {
-    const counters = document.querySelectorAll(".counter");
+  const counters = document.querySelectorAll(".counter");
 
-    counters.forEach(counter => {
-        let target = +counter.getAttribute("data-target");
-        let count = 0;
-        let speed = target / 100; // Điều chỉnh tốc độ đếm
+  counters.forEach((counter) => {
+    let target = +counter.getAttribute("data-target");
+    let count = 0;
+    let speed = target / 100; // Điều chỉnh tốc độ đếm
 
-        function updateCount() {
-            count += speed;
-            if (count < target) {
-                counter.innerText = Math.floor(count);
-                requestAnimationFrame(updateCount);
-            } else {
-                counter.innerText = target;
-            }
-        }
-        updateCount();
-    });
+    function updateCount() {
+      count += speed;
+      if (count < target) {
+        counter.innerText = Math.floor(count);
+        requestAnimationFrame(updateCount);
+      } else {
+        counter.innerText = target;
+      }
+    }
+    updateCount();
+  });
 };
+
+//Scroll
+document.getElementById("scrollBtn").addEventListener("click", function () {
+  document
+    .getElementById("next-section")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+
+
+
